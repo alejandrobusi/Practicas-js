966d548150f3e0f2552f4ca20fe6433831b6adb6
AlejandroBusi <alejandro_busi@hotmail.com>
2021-05-28 19:21:19 -0300

    cambios menores

--- Js/app.js
+++ Js/app.js
@@ -1,3 +1,9 @@
-function alertar(params) {
+function alertar() {
   alert('Este boton llama a la Funcion "Alertar"');
+}
+function Suma() {
+  let numberone = document.getElementById("imputej3a").value;
+  let numbertwo = document.getElementById("imputej3b").value;
+  let sumar = (parseInt(numberone) + parseInt(numbertwo));
+  alert("El resultado de la suma es " + sumar);
 }
